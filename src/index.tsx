241aa7122e2e3f7888aec4b2218c4cde28e96971
Karam Jammal <karamjammal6@gmail.com>
2022-01-09 17:23:28 +0400

    export Style & theme types

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,18 @@ import StyleSheet from './core/styleSheet';
 // Import appearanceHook
 import appearanceHook from './core/appearance';
 
+// Import types
+import type { TImageStyle, TTextStyle, TViewStyle, themeTypes } from './types';
+
 /**
  * Export All
  */
-export { t, StyleSheet, appearanceHook };
+export {
+  t,
+  StyleSheet,
+  appearanceHook,
+  TImageStyle,
+  TTextStyle,
+  TViewStyle,
+  themeTypes,
+};
